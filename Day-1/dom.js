bcc5e89bce5a8592d591863353d804c6b1e3dcc7
Raamponsah <raamponsah@gimpa.edu.gh>
2024-08-06 14:12:12 +0000

    day2

---
+++ Day-1/dom.js
@@ -0,0 +1,67 @@
+// Studying the Document Object Model
+// yesterday's assignment
+let blog = {
+    posts:[
+        {
+        title: "Ghana is in a debate",
+        content:"lorem ipsum dolor sit amet, consectetur adip",
+        publishedDate:"23-09-2015",
+        draftStatus:"published",
+        author:"John Penner",
+        coverImage:"http://placehoder.com/images/200x200.png",
+    },
+    {
+        title:"Learning JS from scratch!",
+        content:"lorem ipsum dolor sit amet, consectet",
+        publishedDate:"23-09-2020",
+        draftStatus:"published",
+        author:"Rhea Seaner",
+        coverImage:"http://placehoder.com/images/200x200.png",
+    },
+    {
+        title:"Babies dont cry",
+        content:"lorem ipsum dolor sit amet, con",
+        publishedDate:"3-06-2021",
+        draftStatus:"pending",
+        coverImage:"http://placehoder.com/images/200x200.png",
+    },
+    {
+        title:"Nii likes standing by head",
+        content: "Lorem ipsupe sjhsdjs",
+        publishedDate: "05-09-2021",
+        draftStatus:"published",
+        author: "Ralph"
+
+    }
+],
+
+autobiographies:[
+    {
+        personOfInterest:"Dr. Kwame Nkrumah",
+        content: "lorem ipsum sig  amemt"
+    },
+    {
+        personOfInterest: "John Agyekum Kuffour",
+        content: "lorem ipsum sdolor sit amet"
+
+    }
+]
+}
+
+let body = document.getElementsByTagName("body")[0]
+
+let ul = document.createElement("ul")
+
+for (let i = 0; i< blog.posts.length; i++) {
+    let li = document.createElement("li")
+    let a = document.createElement("a")
+    a.setAttribute("href","/blog/posts"+blog.posts[i].title)
+    a.textContent =blog.posts[i].title
+    li.appendChild(a)
+    ul.appendChild(li)
+}
+
+body.appendChild(ul)
+
+
+body.appendChild(h1)
--- Day-1/main.js
+++ Day-1/main.js
@@ -1,2 +1,49 @@
-let body  = document.getElementsByTagName('body');
+// excercies on Array
+// generate an array of fruits
+// always find the last fruit
+let fruits = ["Pear","Pine","Mango","Cherry","Watermelon"]
+let lastFruit = fruits[fruits.length-1] ///fruits[4]//output: watermelon
+console.log(lastFruit)
+
+// 2
+// With this array, always find the middle item
+// [Hint] write a function for this ensure it is for all odd arrays
+// ["a", "b", "c", "d", "e"]
+let letters = ["a", "b", "c", "d", "e","f"]
+
+function findMiddleItem(arr){
+        let arrLength = arr.length
+        if(arrLength % 2 == 0){
+            console.log("It is even ")
+        }else{
+            let middlePositionValue = Math.floor(arr.length/2)
+            console.log(middlePositionValue)
+            console.log(arr[middlePositionValue])
+        }
+       
+}
+
+// findMiddleItem(letters);
+
+
+
+// 3
+//  remove the last item and place it as the first item
+// [Hint] you'll need a function
+// ["a", "b", "c", "d", "e"]
+
+function switchLastWithFirst(arr){
+    let last = arr[arr.length-1]
+    let first =arr[0]
+    arr[0] = last;
+    arr[arr.length-1] = first
+    console.log(arr)
+}
+
+switchLastWithFirst(["Pear","Pine","Mango","Cherry","Watermelon"])
+
+// 4
+// create an object with two arrays; one of girl names and the other boy names
+// write a function that pair the names up into another array
+// You''ll need one array with sub arrays at the end
 
